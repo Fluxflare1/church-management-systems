b6fe5f88d6d4aa2a23eb02eaf54ef7d1dfc1a1bc
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-22 21:07:39 +0100

    Create fetcher.ts

---
+++ apps/web/public-site/lib/fetcher.ts
@@ -0,0 +1,25 @@
+// apps/web/public-site/lib/fetcher.ts
+export const apiBase = process.env.NEXT_PUBLIC_API_URL || 'https://api.thogmi.org';
+
+export async function fetchJSON<T = any>(url: string, options: RequestInit = {}) : Promise<T> {
+  const res = await fetch(url, {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      ...(options.headers || {}),
+    },
+    // allow Next.js ISR revalidate via options.next if used in server components
+  });
+
+  if (!res.ok) {
+    const text = await res.text().catch(() => '');
+    const err = new Error(`Fetch error: ${res.status} ${res.statusText} ${text}`);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    err.status = res.status;
+    throw err;
+  }
+
+  return (await res.json()) as T;
+}
