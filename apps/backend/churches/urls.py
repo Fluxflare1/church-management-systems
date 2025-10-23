5154b664128053da2e9c7e19fd8dd6992c252bbb
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-23 17:05:49 +0100

    Delete apps/backend/churches directory

--- apps/backend/churches/urls.py
+++
@@ -1,7 +0,0 @@
-# apps/backend/churches/urls.py
-from django.urls import path
-from .views import BranchListView
-
-urlpatterns = [
-    path('branches/', BranchListView.as_view(), name='branch-list'),
-]
