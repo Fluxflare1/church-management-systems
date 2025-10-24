db027f6ca762f1160adc3d22766b48705d3b3ef5
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-24 12:26:43 +0100

    Create index.ts

---
+++ apps/web/lib/communications/components/index.ts
@@ -0,0 +1,5 @@
+export { NotificationCenter } from './notification-center';
+export { ChatInterface } from './chat-interface';
+export { TemplateBuilder } from './template-builder';
+export { CampaignDashboard } from './campaign-dashboard';
+export { AudienceBuilder } from './audience-builder';
