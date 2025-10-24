a2018cdbd8475a764bdb24468b5cd9c3f485a209
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-24 11:19:20 +0100

    Create index.ts

---
+++ apps/web/lib/communications/index.ts
@@ -0,0 +1,18 @@
+// Types
+export * from './types';
+
+// API Client
+export { communicationApi } from './api-client';
+
+// Hooks
+export * from './hooks/use-templates';
+export * from './hooks/use-campaigns';
+export * from './hooks/use-messaging';
+export * from './hooks/use-conversations';
+export * from './hooks/use-preferences';
+export * from './hooks/use-channels';
+export * from './hooks/use-analytics';
+export * from './hooks/use-websocket';
+
+// Components
+export { NotificationCenter } from './components/notification-center';
